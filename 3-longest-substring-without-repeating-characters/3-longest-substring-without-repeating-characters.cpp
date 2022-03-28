1b5448213f0702da772bfcdae339bbe9aa8c227a
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-03-29 00:07:33 +0530

    Time: 21 ms (57.92%), Space: 8.2 MB (72.50%) - LeetHub

--- 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.cpp
@@ -1,36 +1,3 @@
-// class Solution {
-// public:
-//     int lengthOfLongestSubstring(string s) {
-//         unordered_map<char,pair<int,int>>mp;
-//         int maxlen=0;
-//         int curlen=0;
-//         if(s.length() == 0) return 0;
-//         for(int i=0; i<s.length();i++){
-//             //p 
-//             if(mp[s[i]].first == 0){
-//                 curlen++;
-//                 mp[s[i]].first++;
-//                 mp[s[i]].second =i;
-                
-//             }
-//             else{
-//                 curlen =curlen - mp[s[i]].second;
-//                 // mp.clear();
-//                 mp[s[i]].second =i;
-                
-//             }
-//             cout<<"curlen "<<curlen<<" ";
-//             maxlen = max(curlen,maxlen);
-//             cout<<"maxlen "<< maxlen<<" "<<"s[i] "<< s[i]<<endl;
-            
-
-            
-
-//         }
-//         return maxlen;
-        
-//     }
-// };
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
