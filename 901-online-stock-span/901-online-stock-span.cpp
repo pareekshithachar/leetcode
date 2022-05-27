c7bb4cea2e5907bdd5e8e199adafada38f0cdcf3
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-05-27 14:54:55 +0530

    Time: 476 ms (6.61%), Space: 86.1 MB (24.85%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,24 +1,26 @@
-class StockSpanner {
+class StockSpanner
+{
 public:
-vector<int> res;
-stack<int> st;
+    vector<int> res;
+    stack<int> st;
 
-    int i=0;
-StockSpanner() {
-}
-
-int next(int price) {
-   
-    res.push_back(price);
-    while(!st.empty() && price >=res[st.top()] )
+    int i = 0;
+    StockSpanner()
     {
-        st.pop();
-        
     }
-    
-    int a =st.empty()?-1:st.top();
-     st.push(i);
-    i++;
-    return i-a-1;
-}
+
+    int next(int price)
+    {
+
+        res.push_back(price);
+        while (!st.empty() && price >= res[st.top()])
+        {
+            st.pop();
+        }
+
+        int a = st.empty() ? -1 : st.top();
+        st.push(i);
+        i++;
+        return i - a - 1;
+    }
 };
