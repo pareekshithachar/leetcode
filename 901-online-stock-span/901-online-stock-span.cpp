062ccfc2da245898eff8377340b5a9505a4636d0
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-05-28 10:33:56 +0530

    Time: 447 ms (9.13%), Space: 84.3 MB (29.81%) - LeetHub

--- 901-online-stock-span/901-online-stock-span.cpp
+++ 901-online-stock-span/901-online-stock-span.cpp
@@ -1,26 +1,42 @@
-class StockSpanner
-{
+class StockSpanner {
 public:
-    vector<int> res;
-    stack<int> st;
-
-    int i = 0;
-    StockSpanner()
-    {
+    int i=0;
+    int ans=0;
+    stack<pair<int,int>>st;
+    
+ 
+    StockSpanner() {
+        
     }
-
-    int next(int price)
-    {
-
-        res.push_back(price);
-        while (!st.empty() && price >= res[st.top()])
-        {
-            st.pop();
+    
+    int next(int price) {
+        
+       while(!st.empty() && price>= st.top().first){
+           
+           st.pop();
+           
+       } 
+        if(st.empty()) ans= i+1;
+        else{
+            ans = i-st.top().second;
         }
-
-        int a = st.empty() ? -1 : st.top();
-        st.push(i);
+        
+        st.push({price,i});
         i++;
-        return i - a - 1;
+        return ans;
+        // while(!st.empty() && st.top()<=price) st.pop();
+        // if(st.empty()) ans=i+1;
+        // else{
+        //     ans= i-st.top();
+        // }
+        // st.push(price);
+        // i++;
+        // return ans;
     }
 };
+
+/**
+ * Your StockSpanner object will be instantiated and called as such:
+ * StockSpanner* obj = new StockSpanner();
+ * int param_1 = obj->next(price);
+ */
