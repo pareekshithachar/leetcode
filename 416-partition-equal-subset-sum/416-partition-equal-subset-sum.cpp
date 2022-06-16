1929cdaf249a50accf61b95a520bc07d41312452
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-06-16 15:58:18 +0530

    Time: 562 ms (13.89%), Space: 75.9 MB (16.82%) - LeetHub

--- 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
+++ 416-partition-equal-subset-sum/416-partition-equal-subset-sum.cpp
@@ -2,31 +2,31 @@ class Solution {
 public:
     bool canPartition(vector<int>& nums) {
         
-        int sum=0;
+        int target=0;
+        int n= nums.size();
         for(int i=0;i<nums.size();i++){
-            sum+=nums[i];
+            target+=nums[i];
         }
         
-        if(sum%2==1) return false;
-        
-            sum=sum/2;
-            vector<vector<int>>dp(nums.size()+1,vector<int>(sum +1));
-            for(int i=0;i<=nums.size();i++){
-                for(int j=0;j<=sum;j++){
+        if(target%2 == 1) return false;
+        target = target/2;
+        vector<vector<int>>dp(nums.size()+1,vector<int>(target+1,0));
+        for(int i=0;i<=nums.size();i++){
+                for(int j=0;j<=target;j++){
                     if(i==0) dp[i][j]=false;
                     else if(j==0) dp[i][j]=true;
                 }
             }
-            for(int i=1;i<=nums.size();i++){
-                for(int j=1;j<=sum;j++){
-                    if(nums[i-1]<=j){
-                        dp[i][j]=dp[i-1][j-nums[i-1]]||dp[i-1][j];
-                    }else{
-                        dp[i][j]=dp[i-1][j];
-                    }
+        for(int i =1;i<=n;i++){
+            for(int j=1;j<=target;j++){
+                if(nums[i-1]<=j){
+                    dp[i][j] = dp[i-1][j-nums[i-1]] || dp[i-1][j];
+                }else{
+                    dp[i][j] = dp[i-1][j];
                 }
             }
+        }
+        return dp[n][target];
         
-        return dp[nums.size()][sum];
     }
 };
