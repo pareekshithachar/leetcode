c72dbb18e6902aa99ee7728f3851c75c54cee6c9
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-08-16 15:05:38 +0530

    Time: 0 ms (100.00%), Space: 5.8 MB (78.71%) - LeetHub

---
+++ 1736-latest-time-by-replacing-hidden-digits/1736-latest-time-by-replacing-hidden-digits.cpp
@@ -0,0 +1,30 @@
+class Solution {
+public:
+    string maximumTime(string time) {
+        if(time[4] == '?'){
+            time[4] ='9';
+        }
+        if(time[3] == '?'){
+            time[3]='5';
+        }
+        if(time[0] == '?'){
+            if(time[1] == '?'){
+                time[0]='2';
+                time[1] ='3';
+                return time;
+            }
+            if(time[1] > '3'){
+                time[0] = '1';
+            }else{
+                time[0]='2';
+            }
+        }else if(time[1] == '?'){
+            if(time[0] != '2' ){
+                time[1] ='9';
+            }else{
+                time[1] = '3';
+            }
+        }
+        return time;
+    }
+};
