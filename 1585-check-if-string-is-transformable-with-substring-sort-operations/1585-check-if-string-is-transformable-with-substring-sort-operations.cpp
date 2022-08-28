bbb1442f8af31ec1686fcc837f4a3b92b27c1927
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-08-28 22:25:03 +0530

    Time: 136 ms (36.36%), Space: 22.8 MB (77.27%) - LeetHub

--- 1585-check-if-string-is-transformable-with-substring-sort-operations/1585-check-if-string-is-transformable-with-substring-sort-operations.cpp
+++ 1585-check-if-string-is-transformable-with-substring-sort-operations/1585-check-if-string-is-transformable-with-substring-sort-operations.cpp
@@ -1,29 +1,27 @@
 class Solution {
 public:
-    vector<vector<int>> pLoc;
-    vector<int> ptr;
-    
-    bool isPossible(string& s, int num){
-        for(int i=0;i<num;i++){
-            // if(ptr[i]==-1) continue;
-            if(ptr[i]>=pLoc[i].size()) continue;
-            if(pLoc[i][ptr[i]]<pLoc[num][ptr[num]]) return false;
+    vector<int>count;
+    vector<vector<int>>indx;
+    bool ispos(string &s, int num){
+        for(int i=0;i < num;i++){
+            if(count[i] >= indx[i].size()) continue;
+            if(indx[i][count[i]] < indx[num][count[num]]) return false;
         }
         return true;
     }
     bool isTransformable(string s, string t) {
-        pLoc.resize(10);//has indices
-        ptr.resize(10,-1); // has count
+        count.resize(10,-1);
+        indx.resize(10);
         
-        for(int i=0;i<s.length();i++) {
-            ptr[s[i]-'0']=0;
-            pLoc[s[i]-'0'].push_back(i);
+        for(int i=0;i<s.size();i++){
+            count[s[i] - '0'] = 0;
+            indx[s[i] - '0'].push_back(i);
         }
         
-        for(int i=0;i<t.length();i++){
-            int num = t[i]-'0';
-            if(ptr[num]==-1) return false;
-            if(isPossible(s,num)) ptr[num]++;
+        for(int i=0;i<t.size();i++){
+            int num = t[i] - '0';
+            if(count[num] == -1) return false;
+            if(ispos(s,num)) count[num]++;
             else return false;
         }
         return true;
