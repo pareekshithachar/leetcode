9570b5fd81f3a0d6566b3ebbe943f2a0f43843f2
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-09-22 19:21:57 +0530

    Time: 7 ms (18.46%), Space: 6.9 MB (21.19%) - LeetHub

--- 216-combination-sum-iii/216-combination-sum-iii.cpp
+++ 216-combination-sum-iii/216-combination-sum-iii.cpp
@@ -2,7 +2,7 @@ class Solution {
 public:
     vector<vector<int>>res;
  
-    void solve(int k, int tempk,int n, set<int> s, int sum,vector<int>ans, int x){
+    void solve(int k, int tempk,int n, int sum,vector<int>ans, int x){
         if(tempk == k) {
             if(sum == n){
                 res.push_back(ans);
@@ -15,7 +15,7 @@ public:
             if(tempk != k){
                 
                 ans.push_back(i);
-                solve(k,tempk+1,n,s,sum + i,ans, i);
+                solve(k,tempk+1,n,sum + i,ans, i);
                 ans.pop_back();
                 
             }
@@ -23,15 +23,12 @@ public:
         return;
     }
     vector<vector<int>> combinationSum3(int k, int n) {
-        if(n>45 || (n==45 && k<9)){
+        if(n>45){
             return {};
         }
-        if(n==45 && k == 9){
-            return {{1,2,3,4,5,6,7,8,9}};
-        }
-        set<int>s;
+        
         vector<int>ans;
-         solve(k,0,n,s,0,ans,0);
+         solve(k,0,n,0,ans,0);
         return res;
     }
 };
