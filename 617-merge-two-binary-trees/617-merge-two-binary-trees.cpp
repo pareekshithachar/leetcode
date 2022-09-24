b1e2c2a095e5b333f63cbc83c703720aa24608c1
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-09-24 15:07:51 +0530

    Time: 86 ms (17.24%), Space: 32.9 MB (23.42%) - LeetHub

---
+++ 617-merge-two-binary-trees/617-merge-two-binary-trees.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* root1, TreeNode* root2) {
+        TreeNode* root;
+        if(!root1 && !root2){
+            return NULL;
+        }
+        else if(root1 && !root2){
+            return root1;
+        }
+        else if(!root1 && root2){
+            return root2;
+        }
+        else{
+            root = new TreeNode(root1->val + root2->val);
+            root->left = mergeTrees(root1->left ,root2->left);
+            root->right = mergeTrees(root1->right, root2->right);
+        }
+        return root;
+    }
+};
