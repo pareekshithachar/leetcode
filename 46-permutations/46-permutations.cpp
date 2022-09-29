48e4ed8e6eba61a26aec22dc50842c30e5bbb60d
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-09-29 23:26:43 +0530

    Time: 5 ms (53.73%), Space: 7.7 MB (55.54%) - LeetHub

---
+++ 46-permutations/46-permutations.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void solve(vector<int>& nums,vector<vector<int>>&ans,int i){
+        if(i>=nums.size()){
+            ans.push_back(nums);
+        }
+        
+        for(int j = i;j<nums.size();j++){
+            swap(nums[i],nums[j]);
+            solve(nums,ans,i+1);
+            swap(nums[i],nums[j]);
+        }
+        return;
+        
+    }
+    vector<vector<int>> permute(vector<int>& nums) {
+        vector<vector<int>>ans;
+        
+        solve(nums,ans,0);
+        return ans;
+    }
+};
