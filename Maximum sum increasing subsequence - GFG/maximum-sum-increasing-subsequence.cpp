9018b899c7766eb0451a0651f028fee77b9bc167
N Pareekshith Achar <59954032+pareekshithachar@users.noreply.github.com>
2022-12-26 21:23:33 +0530

    Added solution - LeetHub

--- Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
+++ Maximum sum increasing subsequence - GFG/maximum-sum-increasing-subsequence.cpp
@@ -7,25 +7,37 @@ class Solution{
 		
 
 	public:
-	int dp[1002][1002];
-	int solve(int arr[], int n, int cur, int prev){
-	    if(cur >= n) return 0;
+	
+// 	int solve(int arr[], int n, int cur, int prev){
+// 	    if(cur >= n) return 0;
 	    
 	    
-	    int ans =0;
-	    if(dp[cur][prev+1] != -1) return dp[cur][prev+1];
-	    if(prev == -1 || arr[prev] < arr[cur]){
-	        ans = arr[cur] + solve(arr,n,cur+1,cur);
-	    }
-	    ans = max(ans, solve(arr,n,cur+1,prev));
+// 	    int ans =0;
+// 	    if(dp[cur][prev+1] != -1) return dp[cur][prev+1];
+// 	    if(prev == -1 || arr[prev] < arr[cur]){
+// 	        ans = arr[cur] + solve(arr,n,cur+1,cur);
+// 	    }
+// 	    ans = max(ans, solve(arr,n,cur+1,prev));
 	    
-	    return dp[cur][prev+1] = ans;
-	}
-	int maxSumIS(int arr[], int n)  
-	{  
+// 	    return dp[cur][prev+1] = ans;
+// 	}
+	int maxSumIS(int a[], int n){  
 	    // Your code goes here
-	    memset(dp,-1,sizeof(dp));
-	    return solve(arr,n,0,-1);
+	    
+	    vector<int>dp(n,1);
+        int ans=0;
+       for(int i=0;i<n;i++){
+
+            dp[i]=a[i];
+            for(int j=0;j<i;j++){
+                if(a[i]>a[j]){
+                   dp[i]=max(dp[j]+a[i],dp[i]);
+                }
+            }
+            ans=max(ans,dp[i]);
+        }
+
+        return ans;
 	}  
 };
 
